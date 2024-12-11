fec6024ec3940a344b300724c4a1c2ea6b4a5fb7
Ajayidorcas218 <ajayidorcas218@gmail.com>
2024-12-11 16:29:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from newspaper import Article
-
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Render!"
+def scrape_article(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    return article.text
+
+@app.route('/scrape', methods=['GET'])
+def scrape():
+    url = 'https://example.com/your-article-url'
+    result = scrape_article(url)
+    return jsonify({'scraped_text': result})
 
 if __name__ == '__main__':
     app.run(debug=True)
